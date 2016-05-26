2a52df9afba1f50702b55f037180e04140c1a169
Vitaly Shvedchenko <vitaly.shvedchenko@uchi.ru>
2016-05-26 23:52:05 +0300

    authors

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525164109) do
+ActiveRecord::Schema.define(version: 20160526091244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "login"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_authors_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true, using: :btree
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "name_ru"
