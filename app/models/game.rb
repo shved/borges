5439e774962aa3216e2e525080918da3be1c69d4
Vitaly Shvedchenko <vitaly.shvedchenko@uchi.ru>
2016-05-29 13:14:51 +0300

    fix

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,9 @@
 class Game < ApplicationRecord
   has_many :game_results
 
-  validates :game_type, inclusion: { in: %w(start_with_letter avoid_letter wrap_the_word) }
+  validates :game_type, inclusion: { in: %w(start_with_letter avoid_letter wrap_word) }
 
   translates :name, :desc
 
-  TYPES = [:start_with_letter, :avoid_letter, :wrap_the_word].freeze
+  TYPES = [:start_with_letter, :avoid_letter, :wrap_word].freeze
 end
