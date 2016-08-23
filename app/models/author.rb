6b36cc5b937738c2f3dd9f7c99e0983b448addfd
Vitaly Shvedchenko <vitaly.shvedchenko@uchi.ru>
2016-08-23 23:22:44 +0300

    fix

--- app/models/author.rb
+++ app/models/author.rb
@@ -19,14 +19,15 @@ class Author < ApplicationRecord
 
   def save_game_session
     if pending_game_session_hex.present?
-      pending = PendingGameSession.find_by(hex: pending_game_session_hex)
-      GameSession.create(
-        author_id: id,
-        game_id: pending.game_id,
-        props: pending.props,
-        text: pending.text
-      )
-      pending.destroy!
+      if pending = PendingGameSession.find_by(hex: pending_game_session_hex)
+        GameSession.create(
+          author_id: id,
+          game_id: pending.game_id,
+          props: pending.props,
+          text: pending.text
+        )
+        pending.destroy!
+      end
       pending_game_session_hex = nil
     end
   end
