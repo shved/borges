884c7f0da686fe777060eb2b92ef0434940d2b44
Vitaly Shvedchenko <vitaly.shvedchenko@uchi.ru>
2016-09-01 04:34:13 +0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,66 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Game.delete_all
+ThemeSuggestion.delete_all
+
+### games
+
+Game.create!(
+  desc_en: "If you want to fuck the sky,\\nteach your penis how to fly.",
+  desc_ru: "Напишите осмысленный текст, не используя одну букву алфавита.\nПостарайтесь не делать глупостей. Выпить и на дискотеку. Строго. Че еще надо молодому?\nВы должны писать не останавливаясь, игра закончится после 5 секунд бездействия.",
+  game_type: "avoid_letter",
+  name_en: "Avoid the letter",
+  name_ru: "Не использовать букву",
+  position: 1,
+  props: [:letter]
+)
+
+Game.create!(
+  desc_en: "If you want to fuck the sky,\\nteach your penis how to fly.",
+  desc_ru: "Напишите осмысленный текст, в котором все слова будут начинаться на определенную букву.\nПостарайтесь не делать глупостей. Выпить и на дискотеку. Строго. Че еще надо молодому?\nВы должны писать не останавливаясь, игра закончится после 5 секунд бездействия. Можно вводить только кириллические символы (такие правила, что поделаешь).",
+  game_type: "start_with_letter",
+  name_en: "Start with the letter",
+  name_ru: "Начинать с буквы",
+  position: 0,
+  props: [:letter]
+)
+
+Game.create!(
+  desc_en: "If you want to fuck the sky,\\nteach your penis how to fly.",
+  desc_ru: "Перед каждым новым предложением вы будете получать определенное слово, которое обязательно должно присутствовать в предложении. Максимальное количество слов в одном предложении — 20.\nПостарайтесь не делать глупостей. Выпить и на дискотеку. Строго. Че еще надо молодому?\nВы должны писать не останавливаясь, игра закончится после 5 секунд бездействия.",
+  game_type: "wrap_word",
+  name_en: "Wrap the word",
+  name_ru: "Вокруг слова (пока не работает)",
+  position: 2,
+  props: [:words]
+)
+
+### game suggestions
+
+ThemeSuggestion.create!(
+  game_types: ["avoid_letter", "start_with_letter", "wrap_word" ],
+  text_en: "Spell the soldier, raped your sister.",
+  text_ru: "Прокляните солдата, изнасиловавшего вашу сестру"
+)
+
+ThemeSuggestion.create!(
+  game_types: ["avoid_letter", "start_with_letter", "wrap_word"],
+  text_en: "Suffer like there is no tommorow.",
+  text_ru: "Страдайте."
+)
+
+ThemeSuggestion.create!(
+  game_types: ["avoid_letter", "start_with_letter", "wrap_word"],
+  text_en: "Fell in love with an elder woman.",
+  text_ru: "Признайтесь в любви пожилой женщине."
+)
+
+ThemeSuggestion.create!(
+  game_types: ["start_with_letter", "avoid_letter", "wrap_word"],
+  text_en: "Under what circumstances you had the fern last time?",
+  text_ru: "При каких обстоятельствах вы в последний раз ели папоротник?"
+)
+
+ThemeSuggestion.create!(
+  game_types => ["start_with_letter", "avoid_letter", "wrap_word"],
+  text_en => "Describe an erotic scene as you are an six ears old girl.",
+  text_ru => "Опишите впечатления от случайно увиденной эротической сцены от лица шестилетней девочки."
+)
